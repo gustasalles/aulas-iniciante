b571b5a0bd4dc11b7f4c52c9dd13435138830d97
Wolf Develloper <wolfdevelloper@gmail.com>
2022-07-11 19:53:26 -0300

    adicionei comentarios

--- src/Executar.java
+++ src/Executar.java
@@ -1,7 +1,8 @@
 public class Executar {
 
     public static void main(String[] args) {
-       // MenorNumero.MenorEntreEles(2,5);
+        MenorNumero.menorEntreEles(2,5);
+        MaiorNumero.maiorEntreEles(2,8);
 
     }
 }
--- src/MenorNumero.java
+++ src/MenorNumero.java
@@ -1,17 +1,12 @@
 public class MenorNumero {
 
-    public static void MenorEntreEles(int n1, int n2) {
-
-
+    public static void menorEntreEles(int n1, int n2) {
+        //isso é uma funcao
         if (n1 < n2) {
-
             System.out.println("o numero" + n1 + "é o menor");
         } else {
-
             System.out.println("o numero" + n2 + "é o menor");
         }
-
-
     }
 
 }
